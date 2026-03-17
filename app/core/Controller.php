2b000c419f6c3dc2ccad5556a0065c467421c612
Samuele Schioppa <samueleschioppa@gmail.com>
2026-03-17 10:39:43 +0100

    files fix

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,16 +2,16 @@
 class Controller {
     // Helper to load a model
     public function model($model) {
-        require_once '../app/models/' . $model . '.php';
+        require_once __DIR__ . '/../models/' . $model . '.php';
         return new $model();
     }
 
     // Helper to load a view and pass data to it
     public function view($view, $data = []) {
-        if (file_exists('../app/views/' . $view . '.php')) {
+        if (file_exists(__DIR__ . '/../views/' . $view . '.php')) {
             // Extract array keys into variables (e.g., $data['malattie'] becomes $malattie)
             extract($data); 
-            require_once '../app/views/' . $view . '.php';
+            require_once __DIR__ . '/../views/' . $view . '.php';
         } else {
             die("View does not exist.");
         }
